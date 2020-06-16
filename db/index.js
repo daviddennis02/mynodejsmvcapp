7f4a86a259b382600156c86680204849b095d51d
daviddennis02 <dennis.david02@gmail.com>
2020-06-16 14:04:22 +0100

    update on the database config file

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
 const { Pool } = require('pg');
 
 // Must have DATABASE_URL in environment variables
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
+const postgresUser = "crud_user";
+const postgresDb = "crud_db";
+const postgresPass = "1019";
+
+var connectionString = `postgres://${postgresUser}:${postgresPass}@localhost:5432/${postgresDb}`;
 
 const pool = new Pool({
     connectionString: connectionString,
